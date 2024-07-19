4b021a0dd080454cf6c1b2e59acb457c830f1759
Klaus Zanders <code@kgz.me>
2024-07-19 20:33:53 +0200

    Add aliases, etc

---
+++ system/path.zsh
@@ -0,0 +1,2 @@
+export PATH="/opt/homebrew/sbin:/opt/homebrew/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:$HOME/.rvm/bin:$HOME/.node/bin:$HOME/go/bin"
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
