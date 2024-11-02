6da6845f0d681c63e193a457bfe4b730c28d64c1
Klaus Zanders <code@kgz.me>
2024-11-02 18:55:29 +0100

    Update stuff

--- homebrew/select-packages.sh
+++ homebrew/select-packages.sh
@@ -3,10 +3,9 @@
 if ! [ -x "$(command -v gum)" ]; then
     brew install gum
 fi
+
 pushd "$(dirname "$(realpath -- "$0")")"
-packages=$(cat packages.txt | gum choose --no-limit --selected=$(cat ~/.selected-homebrew-crates) | paste -sd "," -)
-
 touch ~/.selected-homebrew-crates
+packages=$(cat packages.txt | gum choose --no-limit --selected=$(cat ~/.selected-homebrew-crates) | paste -sd "," -)
 echo $packages > ~/.selected-homebrew-crates
-
 popd
