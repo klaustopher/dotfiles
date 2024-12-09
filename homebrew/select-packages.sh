011599632ea5f92bffdbf84e1cf0b6bb5572043e
Klaus Zanders <klaustopher@users.noreply.github.com>
2024-12-09 12:04:48 +0100

    Update select-packages.sh

--- homebrew/select-packages.sh
+++ homebrew/select-packages.sh
@@ -6,7 +6,7 @@ fi
 
 pushd "$(dirname "$(realpath -- "$0")")"
 touch ~/.selected-homebrew-crates
-packages=$(cat packages.txt | gum choose --no-limit --selected="$(cat ~/.selected-homebrew-crates)" | paste -sd "," -)
+packages=$(cat packages.txt | /opt/homebrew/bin/gum choose --no-limit --selected="$(cat ~/.selected-homebrew-crates)" | paste -sd "," -)
 
 if [ -n "${packages}" ]; then
     echo $packages > ~/.selected-homebrew-crates
