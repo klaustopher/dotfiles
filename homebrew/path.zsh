32a747cb4f7216a2fd8325b6ecff98fca5da4989
Klaus Zanders <code@kgz.me>
2025-01-14 17:06:36 +0100

    Update to pg17

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,1 +1,1 @@
-export PATH="/opt/homebrew/opt/postgresql@16/bin:/opt/homebrew/sbin:/opt/homebrew/bin:$PATH"
+export PATH="/opt/homebrew/opt/postgresql@17/bin:/opt/homebrew/sbin:/opt/homebrew/bin:$PATH"
